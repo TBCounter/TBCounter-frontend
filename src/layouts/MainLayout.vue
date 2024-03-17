9f9af5870e0110ce3032d70e08d2ede1808eddc9
Ivan Riazanov <omega19961@gmail.com>
2024-03-17 19:05:26 +0600

    added dialog window with steps how to use

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -39,10 +39,46 @@
             </q-item-section>
           </q-item>
         </div>
-
-
-
       </q-list>
+      <div class="row justify-center q-mt-md">
+
+        <q-btn color="white" icon="help_outline" @click="help = true" text-color="black" label="Help" />
+        <q-dialog v-model="help">
+          <q-card>
+            <q-card-section class="row items-center q-pb-none">
+              <div class="text-h6">How to use?</div>
+              <q-space />
+              <q-btn icon="close" flat round dense v-close-popup />
+            </q-card-section>
+
+            <q-card-section>
+              <q-list bordered separator padding class="rounded-borders">
+                <q-item>
+                  1. Create an account
+                </q-item>
+                <q-item>
+                  2. Add&nbsp;<a target="_blank"
+                    href="https://chromewebstore.google.com/detail/count-starter/bpinmpkjgndaaheogageogdkeilggieo?hl=ru">chrome
+                    extension</a>&nbsp;to your browser
+                </q-item>
+                <q-item>
+                  3. Open&nbsp;<a target="_blank" href="https://totalbattle.com">game</a>, authorize, open extension
+                  and login
+                </q-item>
+                <q-item>
+                  4. Choose account, click collect and start
+                </q-item>
+                <q-item>
+                  5. Wait until you get kicked out of the game
+                </q-item>
+                <q-item>
+                  6. You can watch how service works on the account page (last screenshot and all chests will appear)
+                </q-item>
+              </q-list>
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+      </div>
     </q-drawer>
 
     <q-page-container>
@@ -55,6 +91,9 @@
 import { ref } from 'vue';
 import { useUser } from '../stores/user'
 import { API_URL } from '../api'
+
+
+const help = ref(false)
 
 const userStore = useUser()
 const leftDrawerOpen = ref(false)
