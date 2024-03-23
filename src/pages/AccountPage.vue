4a460b3faa2807f297176e93194939a5f2bc2e84
Ivan Riazanov <omega19961@gmail.com>
2024-03-23 23:56:15 +0600

    added first attempt to chests

---
+++ src/pages/AccountPage.vue
@@ -0,0 +1,118 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-table
+      class="table--sticky"
+      style="height: calc(100vh - 100px)"
+      title="Chests"
+      virtual-scroll
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :loading="isLoading"
+      :rows-per-page-options="[0]"
+      :virtual-scroll-item-size="25"
+      @virtual-scroll="onScroll"
+    />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { WS_URL } from '../api';
+import { getList } from 'src/api';
+import { Component } from 'vue';
+
+const route = useRoute();
+const wsConnection = ref<WebSocket>();
+
+const isLoading = ref(true);
+
+const nextPage = ref(1);
+const lastPage = ref(0);
+
+const columns = [
+  { label: 'Имя', name: 'player', field: 'player' },
+  { label: 'Сундук', field: 'chest_type', name: 'chest_type' },
+  { label: 'Имя сундука', field: 'chest_name', name: 'chest_name' },
+  { label: 'Время прихода', field: 'got_at', name: 'got_at' },
+  { label: 'Время открытия', field: 'opened_in', name: 'opened_in' },
+];
+
+const rows = ref([]);
+watch(
+  () => route.params.id,
+  (value) => {
+    wsConnection.value?.close();
+    console.log(value);
+    updateChestsOpenWS();
+  },
+  { immediate: true }
+);
+
+async function onScroll(details: {
+  index: number;
+  from: number;
+  to: number;
+  direction: 'increase' | 'decrease';
+  ref: Component;
+}) {
+  console.log(details);
+  const lastIndex = rows.value.length - 1;
+  if (
+    isLoading.value !== true &&
+    nextPage.value < lastPage.value &&
+    details.to === lastIndex
+  ) {
+    isLoading.value = true;
+
+    console.log(nextPage);
+  }
+  return;
+}
+
+async function updateChestsOpenWS() {
+  wsConnection.value = new WebSocket(WS_URL + 'chests/' + route.params.id);
+  wsConnection.value.onmessage = async function (event) {
+    const data = JSON.parse(event.data);
+    console.log('message', data);
+    rows.value = data.chests;
+    isLoading.value = false;
+    nextPage.value = data.page + 1;
+    lastPage.value = data.total_pages;
+
+    // if (!sorting.value && page.value == 1) {
+    //   const data = JSON.parse(event.data);
+    //   chests.value = data.chests;
+    //   total.value = data.total;
+    //   today.value = data.today_chests;
+    //   await getAccountStateImage(acc.value!.id).then((response) => {
+    //     if (statusImage.value && response) {
+    //       statusImage.value = '';
+    //     }
+    //     statusImage.value = response;
+    //   });
+    // }
+  };
+
+  wsConnection.value.onopen = function (event) {
+    console.log(event);
+    // console.log("Successfully connected to the echo websocket server...")
+  };
+}
+</script>
+
+<style lang="scss">
+.table {
+  &--sticky {
+    thead tr th {
+      position: sticky;
+      z-index: 1;
+      background-color: white;
+    }
+    thead tr:first-child th {
+      top: 0;
+    }
+  }
+}
+</style>
